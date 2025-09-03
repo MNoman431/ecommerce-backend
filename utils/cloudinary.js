1eb2aea14a87ea3bea5b61708be15e75bcfeb422
MNoman431 <nomanrehann112233@gmail.com>
2025-09-04 01:12:51 +0500

    Cloudinary.js corrected

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("Cloudinary ENV Check:", {
-  CLOUD_NAME: process.env.CLOUD_NAME,
-  CLOUD_API_KEY: process.env.CLOUD_API_KEY,
-  CLOUD_API_SECRET: process.env.CLOUD_API_SECRET ? "✅ Loaded" : "❌ Missing",
-});
+// console.log("Cloudinary ENV Check:", {
+//   CLOUD_NAME: process.env.CLOUD_NAME,
+//   CLOUD_API_KEY: process.env.CLOUD_API_KEY,
+//   CLOUD_API_SECRET: process.env.CLOUD_API_SECRET ? "✅ Loaded" : "❌ Missing",
+// });
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
